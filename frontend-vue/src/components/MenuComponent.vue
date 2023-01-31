d470f66f0b9fefc19d2e9b54f5b0425dea0e8a26
LiberatoDaniel <liberatod956@gmail.com>
2023-01-30 23:43:08 -0300

    menu component

---
+++ frontend-vue/src/components/MenuComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">DanielDEV</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item :to="'/'">Serviços</b-nav-item>
+          <b-nav-item :to="'/ordem-servicos'">Ordem de Serviços</b-nav-item>
+          <b-nav-item :to="'/cadastrar-cliente'" >Cadastrar Clientes</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MenuComponent',
+}
+</script>
